1d2b675190d427430df8544e6ad104ab65401bc5
kbh0581 <kbh0581@gmail.com>
2022-08-12 00:28:28 +0900

    first commit

---
+++ src/main/java/com/chalchal/chalchalsever/ChalChalSeverApplication.java
@@ -0,0 +1,13 @@
+package com.chalchal.chalchalsever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalChalSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChalChalSeverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chalchal/chalchalsever/ChalChalSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chalchal.chalchalsever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalChalSeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

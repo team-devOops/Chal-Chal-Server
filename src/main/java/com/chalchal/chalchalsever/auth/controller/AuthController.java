846f05076c150c304aef51e9790a8ab8602aec46
zinzo <zinzoddari@gmail.com>
2022-08-25 16:28:01 +0900

    swagger-ui 설정

--- src/main/java/com/chalchal/chalchalsever/auth/controller/AuthController.java
+++ src/main/java/com/chalchal/chalchalsever/auth/controller/AuthController.java
@@ -1,13 +1,21 @@
 package com.chalchal.chalchalsever.auth.controller;
 
+import com.chalchal.chalchalsever.domain.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@Api(tags = {"회원가입"})
 @RequiredArgsConstructor
 public class AuthController {
-
-
+//    @PostMapping(value = "/join")
+//    @ApiOperation(value = "회원가입")
+//    public User signUp() {
+//        return null;
+//    }
 }

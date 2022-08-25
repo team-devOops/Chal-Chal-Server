f355a5e9388ef44e652c1d1f4b997dd2f7906329
zinzo <zinzoddari@gmail.com>
2022-08-25 17:58:29 +0900

    Consider defining a bean of type 'org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder' in your configuration. 오류 뜸

--- src/main/java/com/chalchal/chalchalsever/auth/repository/UserRepository.java
+++ src/main/java/com/chalchal/chalchalsever/auth/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.chalchal.chalchalsever.auth.repository;
 
 import com.chalchal.chalchalsever.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findById(String id);
+
+    User findByIdAndPw(String id, String pw);
 }
--- src/main/java/com/chalchal/chalchalsever/config/security/WebSecurityConfig.java
+++ src/main/java/com/chalchal/chalchalsever/config/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.chalchal.chalchalsever.config.security;
 import com.chalchal.chalchalsever.config.jwt.JwtAuthenticationFilter;
 import com.chalchal.chalchalsever.config.jwt.JwtConfig;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -33,5 +36,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }

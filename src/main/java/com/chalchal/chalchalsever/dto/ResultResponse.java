2868a90ef87186ca4896f8cf553ed7aae67b56cf
zinzoddari <zinzoddari@gmail.com>
2022-09-08 23:18:01 +0900

    ResultResponse 수정

--- src/main/java/com/chalchal/chalchalsever/dto/ResultResponse.java
+++ src/main/java/com/chalchal/chalchalsever/dto/ResultResponse.java
@@ -4,12 +4,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
+
 @Builder
 @AllArgsConstructor
-public class ResultResponse {
+public class ResultResponse<T> {
+
     @JsonProperty
     private int status;
 
     @JsonProperty
     private String message;
+
+    @JsonProperty
+    private T data;
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }

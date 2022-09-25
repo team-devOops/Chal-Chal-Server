0c8c3760fdc5190ea394b902707286823a5690a7
zinzoddari <zinzoddari@gmail.com>
2022-09-25 23:41:20 +0900

    불필요한 부분 삭제

--- src/main/java/com/chalchal/chalchalsever/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/chalchal/chalchalsever/config/jwt/JwtAuthenticationFilter.java
@@ -29,7 +29,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
         if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
             Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-            log.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+            log.debug("Security Context : '{}', uri: {}", authentication.getName(), requestURI);
         }
         else {
             log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);

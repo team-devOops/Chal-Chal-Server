25e9beab3df73e17a530516f004bbc206da69c7e
zinzoddari <zinzoddari@gmail.com>
2022-10-09 23:57:16 +0900

    mail domain 추가

--- src/main/java/com/chalchal/chalchalsever/domain/Mail.java
+++ src/main/java/com/chalchal/chalchalsever/domain/Mail.java
@@ -2,6 +2,27 @@ package com.chalchal.chalchalsever.domain;
 
 import lombok.Builder;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Builder
 public class Mail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "seq_no", nullable = false)
+    private Long seqNo;
+
+    @Column(name = "from")
+    private String from;
+
+    @Column(name = "to")
+    private String to;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "text")
+    private String text;
 }

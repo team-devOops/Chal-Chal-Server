575624b714cdb3c77a399f133ae592de6f4d037d
zinzoddari <zinzoddari@gmail.com>
2022-12-21 00:36:40 +0900

    test: 인증번호 관련 테스트 코드 생성

---
+++ src/test/java/com/chalchal/chalchalsever/auth/AuthTest.java
@@ -0,0 +1,24 @@
+package com.chalchal.chalchalsever.auth;
+
+import com.chalchal.chalchalsever.domain.MailAuthNum;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class AuthTest {
+
+    @Test
+    void 인증번호_자릿수_오류() {
+        String randomNum = "1234567";
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            MailAuthNum mailAuthNum = new MailAuthNum(randomNum);
+        });
+    }
+
+    @Test
+    void 인증번호_비교_실패() {
+        String randomNum = "123456";
+        assertThat(randomNum).isNotEqualTo("111111");
+    }
+}

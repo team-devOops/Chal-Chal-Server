b71adfe9542aa503cfb3c6203e57c2723f71af8c
zinzoddari <zinzoddari@gmail.com>
2022-12-21 00:36:26 +0900

    fix: 메일 인증번호 객체 관련 생성

---
+++ src/main/java/com/chalchal/chalchalsever/domain/MailAuthNum.java
@@ -0,0 +1,17 @@
+package com.chalchal.chalchalsever.domain;
+
+public class MailAuthNum {
+    public static final int MAX_AUTH_NUM = 6;
+    private final String authNum;
+
+    public MailAuthNum(String authNum) {
+        this.authNum = validateAuthNum(authNum);
+    }
+
+    private String validateAuthNum(String authNum) {
+        if(MAX_AUTH_NUM == authNum.length()) return authNum;
+        throw new IllegalArgumentException("[ERROR] " + MAX_AUTH_NUM + "자 여야 합니다.");
+    }
+
+
+}

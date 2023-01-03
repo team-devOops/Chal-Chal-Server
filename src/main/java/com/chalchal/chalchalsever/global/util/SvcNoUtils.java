36b328ee34df548d2420099262c8f36434d48a33
zinzoddari <zinzoddari@gmail.com>
2023-01-03 17:10:10 +0900

    feat: 고유키값 svc_no 관련 작성

--- src/main/java/com/chalchal/chalchalsever/global/util/SvcNoUtils.java
+++ src/main/java/com/chalchal/chalchalsever/global/util/SvcNoUtils.java
@@ -1,14 +1,25 @@
 package com.chalchal.chalchalsever.global.util;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class SvcNoUtils {
+
+    private static final int SVC_NO_MAX_SIZE = 32;
+
+    private static String svcNoType;
+
+    @Value("${svcno.type}")
+    private void setSvcNoType(String type) {
+        this.svcNoType = type;
+    }
+
     public static String getSvcNo() {
-        return DateUtils.getCurrentDay("yyyyMMddHHmmssSSS") + (int)((Math.random()*10000));
+        String svcNo = DateUtils.getCurrentDay("yyyyMMddHHmmssSSS") + svcNoType;
+
+        return String.format(svcNo + "%0" + (SVC_NO_MAX_SIZE - svcNo.length()) + "d", (int)(Math.random() * (Integer.MAX_VALUE))).substring(0, SVC_NO_MAX_SIZE);
     }
 }
--- src/test/java/com/chalchal/chalchalsever/SvcNoTest.java
+++ src/test/java/com/chalchal/chalchalsever/SvcNoTest.java
@@ -2,10 +2,36 @@ package com.chalchal.chalchalsever;
 
 import com.chalchal.chalchalsever.global.util.SvcNoUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
 public class SvcNoTest {
     @Test
     void getSvcNo() {
         System.out.println(SvcNoUtils.getSvcNo());
+    }
+
+    @Test
+    void 중복_svc_no_테스트() {
+        Map<String, Integer> svcNoMap = new HashMap<>();
+
+        for (int i = 0; i < 100; i++) {
+            String svcNo = SvcNoUtils.getSvcNo();
+
+            if(svcNoMap.get(svcNo) != null) {
+                int temp = svcNoMap.get(svcNo) + 1;
+                svcNoMap.put(svcNo, temp);
+            }
+            else {
+                svcNoMap.put(svcNo, 0);
+            }
+        }
+
+        for (int i = 0; i < svcNoMap.size(); i++) {
+            System.out.println(svcNoMap.values());
+        }
     }
 }

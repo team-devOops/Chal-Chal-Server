06591512d4f278ca010fd89d01ed21d55f93d8f9
zinzoddari <zinzoddari@gmail.com>
2023-01-07 03:04:27 +0900

    style: 대문자로 변경 및 코멘트 추가

--- src/main/java/com/chalchal/chalchalsever/domain/Mail.java
+++ src/main/java/com/chalchal/chalchalsever/domain/Mail.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
 
@@ -11,22 +12,28 @@ import javax.persistence.*;
 @Getter
 @Entity
 @AllArgsConstructor
-@Table(name = "t_mail_history")
+@Table(name = "T_MAIL_HISTORY")
 @NoArgsConstructor
 public class Mail extends BaseDomain {
+
     @Id
-    @Column(name = "svc_no", nullable = false)
+    @Comment("서비스번호")
+    @Column(name = "SVC_NO", nullable = false)
     private String svcNo;
 
-    @Column(name = "from_mail")
+    @Comment("보낸 이메일")
+    @Column(name = "FROM_MAIL")
     private String fromMail;
 
-    @Column(name = "to_mail")
+    @Comment("받는 이메일")
+    @Column(name = "TO_MAIL")
     private String toMail;
 
-    @Column(name = "subject")
+    @Comment("제목")
+    @Column(name = "SUBJECT")
     private String subject;
 
-    @Column(name = "content")
+    @Comment("내용")
+    @Column(name = "CONTENT")
     private String content;
 }
--- src/main/java/com/chalchal/chalchalsever/domain/UserJoinAuth.java
+++ src/main/java/com/chalchal/chalchalsever/domain/UserJoinAuth.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
 
@@ -11,29 +12,36 @@ import javax.persistence.*;
 @Getter
 @Entity
 @AllArgsConstructor
-@Table(name = "t_user_join_auth")
+@Table(name = "T_USER_JOIN_AUTH")
 @NoArgsConstructor
 public class UserJoinAuth extends BaseDomain {
 
     @Id
-    @Column(name = "req_svc_no")
+    @Comment("서비스번호")
+    @Column(name = "SVC_NO")
     private String reqSvcNo;
 
-    @Column(name = "id", nullable = false)
+    @Comment("유저 KEY")
+    @Column(name = "ID", nullable = false)
     private Long id;
 
-    @Column(name = "send_email", nullable = false)
+    @Comment("받는 이메일")
+    @Column(name = "SEND_EMAIL", nullable = false)
     private String sendEmail;
 
-    @Column(name = "auth_code", nullable = false)
+    @Comment("인증코드")
+    @Column(name = "AUTH_CODE", nullable = false)
     private String authCode;
 
-    @Column(name = "valid_date", nullable = false)
+    @Comment("인증유효날짜")
+    @Column(name = "VALID_DATE", nullable = false)
     private String validDate;
 
-    @Column(name = "valid_time", nullable = false)
+    @Comment("인증유효시간")
+    @Column(name = "VALID_TIME", nullable = false)
     private String validTime;
 
-    @Column(name = "auth_yn", nullable = false)
+    @Comment("인증여부")
+    @Column(name = "AUTH_YN", nullable = false)
     private String authYn;
 }

d0132a0ba2048ecdab12ea1e312280cd8eec9d8d
zinzoddari <zinzoddari@gmail.com>
2023-01-09 23:33:43 +0900

    test: StringUtils 관련 BDD 형식으로 수정 및 추가

--- src/test/java/com/chalchal/chalchalsever/StringUtilsTest.java
+++ src/test/java/com/chalchal/chalchalsever/StringUtilsTest.java
@@ -8,13 +8,36 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class StringUtilsTest {
 
     @Test
-    void 빈문자열_체크() {
-        assertThat(StringUtils.isNotEmpty("")).isEqualTo(false);
-        assertThat(StringUtils.isNotEmpty(null)).isEqualTo(false);
+    void 빈문자열_isNotEmpty_체크() {
+        String str1 = "";
+        String str2 = null;
+
+        boolean result1 = StringUtils.isNotEmpty(str1);
+        boolean result2 = StringUtils.isNotEmpty(str2);
+
+        assertThat(result1).isEqualTo(false);
+        assertThat(result2).isEqualTo(false);
     }
 
     @Test
-    void 문자열_체크() {
+    void 문자열_isNotEmpty_체크() {
         assertThat(StringUtils.isNotEmpty("zz")).isEqualTo(true);
+    }
+
+    @Test
+    void 빈문자열_isEmpty_체크() {
+        String str1 = "";
+        String str2 = null;
+
+        boolean result1 = StringUtils.isEmpty(str1);
+        boolean result2 = StringUtils.isEmpty(str2);
+
+        assertThat(result1).isTrue();
+        assertThat(result2).isTrue();
+    }
+
+    @Test
+    void 문자열_isEmpty_체크() {
+        assertThat(StringUtils.isNotEmpty("zz")).isFalse();
     }
 }

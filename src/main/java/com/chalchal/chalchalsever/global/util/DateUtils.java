6186ffb4d76916ac3fa8a36cf4f3776d627adefb
zinzoddari <zinzoddari@gmail.com>
2023-01-11 17:17:36 +0900

    feat: 날짜 관련 계산하는 메소드 추가

--- src/main/java/com/chalchal/chalchalsever/global/util/DateUtils.java
+++ src/main/java/com/chalchal/chalchalsever/global/util/DateUtils.java
@@ -4,13 +4,21 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+/**
+ * 날짜 관련 util 정의한 class입니다.
+ * 해당 Util의 기본 포맷은 [ yyyyMMddHHmmss ] 입니다.
+ *
+ * @author zinzoddari
+ */
 public class DateUtils {
 
     /** Date format */
     public static final String DATE_PATTERN = "yyyy-MM-dd";
     public static final String DATE_PATTERN_FORMAT = "yyyyMMdd";
     /** Date Time format */
-    public static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm";
+    public static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String DATE_TIME_FORMAT = "HHmmss";
 
     /**
      * 현재 날짜를 가져온다.
@@ -44,7 +52,116 @@ public class DateUtils {
      */
     public static String getCurrentTime(String pattern) {
         DateTime dt = new DateTime();
+        return getDateByPattern(dt, pattern);
+    }
+
+    /**
+     * 입력된 날짜에서 day만큼 날을 더한다.
+     * @param date 입력된 특정 날짜(YYYYMMDD)
+     * @param day 더하고자 하는 날짜
+     * @return 특정 날짜에서 day만큼 더해진 날짜 값
+     */
+    public static String plusDayByDate(String date, int day) {
+        return plusDayByDate(date, day, DATE_PATTERN_FORMAT);
+    }
+
+    /**
+     * 입력된 날짜에서 day만큼 날을 더한다.
+     * @param date 입력된 특정 날짜
+     * @param day 더하고자 하는 날짜
+     * @param pattern 날짜 패턴
+     * @return 특정 날짜에서 day만큼 더해진 날짜 값
+     */
+    public static String plusDayByDate(String date, int day, String pattern) {
+        DateTime dt = DateTime.parse(date, DateTimeFormat.forPattern(pattern));
+        return getDateByPattern(dt.plusDays(day), pattern);
+    }
+
+    /**
+     * 현재 날짜에서 day만큼 날을 더한다.
+     * @param day 더하고자 하는 날짜
+     * @return 현재 날짜에서 day만큼 더해진 날짜 값
+     */
+    public static String plusDayByCurrentDay(int day) {
+        return plusDayByDate(getCurrentDay(DATE_PATTERN_FORMAT), day);
+    }
+
+    /**
+     * 입력된 날짜에서 month만큼 월을 더한다.
+     * @param month 더하고자 하는 개월 수
+     * @return 특정 날의 월이 month만큼 더해진 날짜 값
+     */
+    public static String plusMonthByDate(String date, int month) {
+        return plusMonthByDate(date, month, DATE_PATTERN_FORMAT);
+    }
+
+    /**
+     * 입력된 날짜에서 month만큼 월을 더한다.
+     * @param month 더하고자 하는 개월 수
+     * @return 특정 날의 월이 month만큼 더해진 날짜 값
+     */
+    public static String plusMonthByDate(String date, int month, String pattern) {
+        DateTime dt = DateTime.parse(date, DateTimeFormat.forPattern(pattern));
+        return getDateByPattern(dt.plusMonths(month), pattern);
+    }
+
+    /**
+     * 현재 날짜에서 month만큼 월을 더한다.
+     * @param month 더하고자 하는 개월 수
+     * @return 특정 날의 월이 month만큼 더해진 날짜 값
+     */
+    public static String plusMonthByCurrentDay(int month) {
+        return plusMonthByDate(getCurrentDay(DATE_PATTERN_FORMAT), month);
+    }
+
+    /**
+     * 입력된 날짜에서 year만큼 년을 더한다.
+     * @param year 더하고자 하는 년 수
+     * @param pattern 날짜 패턴
+     * @return 특정 날의 월이 year만큼 더해진 날짜 값
+     */
+    public static String plusYearByDate(String date, int year, String pattern) {
+        DateTime dt = DateTime.parse(date, DateTimeFormat.forPattern(pattern));
+        return getDateByPattern(dt.plusYears(year), pattern);
+    }
+
+    /**
+     * 입력된 날짜에서 year만큼 년을 더한다.
+     * @param year 더하고자 하는 년 수
+     * @return 특정 날의 월이 year만큼 더해진 날짜 값
+     */
+    public static String plusYearByDate(String date, int year) {
+        return plusYearByDate(date, year, DATE_PATTERN_FORMAT);
+    }
+
+    /**
+     * 현재 날짜에서 year만큼 년을 더한다.
+     * @param year 더하고자 하는 개월 수
+     * @return 특정 날의 년이 year만큼 더해진 날짜 값
+     */
+    public static String plusYearByCurrentDay(int year) {
+        return plusYearByDate(getCurrentDay(DATE_PATTERN_FORMAT), year);
+    }
+
+    /**
+     * 주어진 날짜 및 시간을 패턴에 맞게 가져온다.
+     * @param date 날짜 및 시간
+     * @param pattern 패턴
+     * @return String pattern에 의한 날짜 및 시간
+     */
+    public static String getDateByPattern(String date, String pattern) {
+        DateTime dt = new DateTime(date);
+        return getDateByPattern(dt, pattern);
+    }
+
+    /**
+     * 주어진 날짜 및 시간을 패턴에 맞게 가져온다.
+     * @param date DateTime
+     * @param pattern 패턴
+     * @return String pattern에 의한 날짜 및 시간
+     */
+    public static String getDateByPattern(DateTime date, String pattern) {
         DateTimeFormatter fmt = DateTimeFormat.forPattern(pattern);
-        return fmt.print(dt);
+        return fmt.print(date);
     }
 }

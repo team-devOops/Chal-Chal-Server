d443850dd312c0443decceec867b85699a557c11
zinzoddari <zinzoddari@gmail.com>
2023-01-15 16:18:23 +0900

    test: 사용하지 않는 Test 삭제

--- src/test/java/com/chalchal/chalchalsever/auth/AuthTest.java
+++ src/test/java/com/chalchal/chalchalsever/auth/AuthTest.java
@@ -4,17 +4,8 @@ import com.chalchal.chalchalsever.domain.auth.entity.UserJoinAuth;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class AuthTest {
-
-    @Test
-    void 인증번호_자릿수_오류() {
-        String randomNum = "1234567";
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            //MailAuthNum mailAuthNum = new MailAuthNum(randomNum);
-        });
-    }
 
     @Test
     void 인증번호_비교_실패() {

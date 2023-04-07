697eccbf7fb59eef7bedb5c2bb124eb11040296e
zinzoddari <zinzoddari@gmail.com>
2023-04-08 03:37:32 +0900

    refactor: TodoTopicController RequestMapping 수정

--- src/main/java/com/chalchal/chalchalserver/todo/controller/TodoTopicController.java
+++ src/main/java/com/chalchal/chalchalserver/todo/controller/TodoTopicController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/todo/topic")
+@RequestMapping("/todo/topics")
 @Api(tags = {"TODO : TOPIC 관리"})
 @RequiredArgsConstructor
 public class TodoTopicController {

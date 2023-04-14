d51911349f9491f3b64cf3138d3de42373662ef9
benggrae <kbh0581@gmail.com>
2023-04-15 00:39:37 +0900

    test: mailAuthNum 테스트 코드 추가.

--- src/main/java/com/chalchal/chalchalserver/auth/domain/MailAuthNum.java
+++ src/main/java/com/chalchal/chalchalserver/auth/domain/MailAuthNum.java
@@ -4,21 +4,31 @@ import com.chalchal.chalchalserver.global.util.RandomUtils;
 
 import java.util.Objects;
 
+/**
+ * 이메일 인증 번호
+ * */
 public class MailAuthNum {
     public static final int MAX_AUTH_NUM = 6;
     private final String authNum;
 
-    private MailAuthNum() {
-        this.authNum = validateAuthNum(RandomUtils.getRandomIntByLength(MAX_AUTH_NUM));
+    public MailAuthNum(String authNum) {
+        validateAuthNum(authNum);
+        this.authNum = authNum;
     }
 
     public static MailAuthNum creteMailAuthNum() {
-        return new MailAuthNum();
+        return new MailAuthNum(RandomUtils.getRandomIntByLength(MAX_AUTH_NUM));
     }
 
-    private static String validateAuthNum(String authNum) {
-        if(MAX_AUTH_NUM == authNum.length()) return authNum;
-        throw new IllegalArgumentException("[ERROR] " + MAX_AUTH_NUM + "자 여야 합니다.");
+    private static void validateAuthNum(String authNum) {
+        if (authNum == null) {
+            throw new IllegalArgumentException("[ERROR] + 이메일 인증번호는 null이면 안된다.");
+        }
+
+        if(MAX_AUTH_NUM != authNum.length()) {
+            throw new IllegalArgumentException("[ERROR] " + MAX_AUTH_NUM + "자 여야 합니다.");
+        }
+
     }
 
     public String value() {

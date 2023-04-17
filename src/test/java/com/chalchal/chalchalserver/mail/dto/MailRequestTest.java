0343e4c122e44ce639ba3997bc69727171b1b218
zinzoddari <zinzoddari@gmail.com>
2023-04-17 17:37:08 +0900

    test: add MailRequest

---
+++ src/test/java/com/chalchal/chalchalserver/mail/dto/MailRequestTest.java
@@ -0,0 +1,80 @@
+package com.chalchal.chalchalserver.mail.dto;
+
+import com.chalchal.chalchalserver.mail.domain.Mail;
+import com.chalchal.chalchalserver.mail.domain.MailDiv;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.SoftAssertions.*;
+
+class MailRequestTest {
+
+    @Test
+    @DisplayName("mailRequest 빈생성자 생성")
+    void mailRequestNoArgsConstructor() {
+        MailRequest request = new MailRequest();
+
+        assertThat(request).isNotNull();
+    }
+
+    @Test
+    @DisplayName("mailRequest 생성")
+    void mailRequest() {
+        //given
+        String svcNo = "svcNo";
+        MailDiv mailDiv = MailDiv.SIGN_UP;
+        String to = "to";
+        String subject = "subject";
+        String content = "content";
+
+        //when
+        MailRequest request = MailRequest.builder()
+                .svcNo(svcNo)
+                .mailDiv(mailDiv)
+                .to(to)
+                .subject(subject)
+                .content(content)
+                .build();
+
+        //then
+        assertSoftly(softAssertions -> {
+            assertThat(request.getSvcNo()).isEqualTo(svcNo);
+            assertThat(request.getMailDiv()).isEqualTo(mailDiv);
+            assertThat(request.getTo()).isEqualTo(to);
+            assertThat(request.getSubject()).isEqualTo(subject);
+            assertThat(request.getContent()).isEqualTo(content);
+        });
+    }
+
+    @Test
+    @DisplayName("mailRequest from")
+    void mailRequestFrom() {
+        //given
+        String svcNo = "svcNo";
+        MailDiv mailDiv = MailDiv.SIGN_UP;
+        String to = "to";
+        String subject = "subject";
+        String content = "content";
+
+        Mail mail = Mail.builder()
+                .svcNo(svcNo)
+                .mailDiv(mailDiv)
+                .toMail(to)
+                .subject(subject)
+                .content(content)
+                .build();
+
+        //when
+        MailRequest request = MailRequest.from(mail);
+
+        //then
+        assertSoftly(softAssertions -> {
+            assertThat(request.getSvcNo()).isEqualTo(svcNo);
+            assertThat(request.getMailDiv()).isEqualTo(mailDiv);
+            assertThat(request.getTo()).isEqualTo(to);
+            assertThat(request.getSubject()).isEqualTo(subject);
+            assertThat(request.getContent()).isEqualTo(content);
+        });
+    }
+}
